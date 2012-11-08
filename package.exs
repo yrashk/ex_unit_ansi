7be5e3c992e1389402e079a35563475bf2e45177
Yurii Rashkovskii <yrashk@gmail.com>
2012-11-08 03:19:35 -0800

    Bump up to 0.0.2

--- package.exs
+++ package.exs
@@ -1,6 +1,6 @@
 Expm.Package.new(name: "ex_unit_ansi", description: "ANSI formatter for ExUnit",
-                 version: "0.0.1", keywords: ["ansi","tty","terminal","unit","test"], 
+                 version: "0.0.2", keywords: ["ansi","tty","terminal","unit","test"], 
                  maintainers: [[name: "Yurii Rashkovskii", 
                                 email: "yrashk@gmail.com"]],
-                 repositories: [[github: "yrashk/ex_unit_ansi", tag: "0.0.1"]])
+                 repositories: [[github: "yrashk/ex_unit_ansi", tag: "0.0.2"]])
     
